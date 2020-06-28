8f9e7c16525834261f4b48c045d510fde7a7551d
Samuel Felipe <samfelgar@gmail.com>
2020-06-27 21:13:50 -0300

    Primeiras alteracoes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\Controller;
 class LoginController extends Controller {
     
     public function index() {
-        return 'Hello World';
+        print phpinfo();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'LoginController@index');
+Route::get('/profiles', 'ProfilesController@index');
